0b81c157d07ff990986a7db5b0c69b3fd15d4577
xatxat <xatma123@gmail.com>
2026-03-02 02:58:39 +0600

    Add more portfolio elements

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ const themeToggleBtn = document.getElementById('theme-toggle');
 const body           = document.body;
 //============================= TRANSLATION - language wrap
 const langEl         = document.querySelector('.langWrap');
-
 //============================= TRANSLATION - language wrap
-//const langEl = document.querySelector('.langWrap');
-        // i got here only 'a' tags from only '.langWrap' class
-        //without involving other 'a' tags here
-        //here is an alternative way to reach the same result:
-        // grab the container…
-        //const langWrap = document.querySelector('.langWrap');
-        // …then only its <a> children
-        //const links = langWrap.querySelectorAll('a');
 const links          = document.querySelectorAll('.langWrap a');
 //variables holds html elements : 
 const logoEl         = document.querySelector('.logo');
@@ -21,7 +12,8 @@ const aboutMe_li     = document.querySelector('.header-left nav ul li:nth-child(
 const skills_li      = document.querySelector('.header-left nav ul li:nth-child(2) a');
 const portfolio_li   = document.querySelector('.header-left nav ul li:nth-child(3) a');
 const videos_li      = document.querySelector('.header-left nav ul li:nth-child(4) a');
-const contacts_li    = document.querySelector('.header-left nav ul li:nth-child(5) a');
+const games_li      = document.querySelector('.header-left nav ul li:nth-child(5) a');
+const contacts_li    = document.querySelector('.header-left nav ul li:nth-child(6) a');
 //about section
 const aboutH2        = document.querySelector('.about h2');
 const aboutP         = document.querySelector('.about p');
@@ -36,6 +28,8 @@ const portfolioH3_2  = pH3List[1];//runner game
 const portfolioH3_3  = pH3List[2];//fight game
 //videos section
 const videosH2       = document.querySelector('.videos h2');
+//games section
+const gamesH2       = document.querySelector('.games h2');
 //contacts section
 const contactsH2     = document.querySelector('.contacts h2');
 const contactsBtn    = document.querySelector('.contact-form button');
@@ -47,15 +41,7 @@ let data; // will hold your translations
 
 // Function to switch themes
 function updateThemeToggleText() {
-    //body.classList.toggle('dark-mode'); // Add/remove dark-mode class from body
-            // Update the button text based on active theme
-    //if (body.classList.contains('dark-mode')) {
-    //    //themeToggleBtn.textContent = "Light Mode";//Dark mode active – show Light Mode option
-    //    themeToggleBtn.textContent = data[attr].theme_toggle_1;
-    //} else {
-    //    //themeToggleBtn.textContent = "Dark Mode";//Light mode active – show Dark Mode option
-    //    themeToggleBtn.textContent = data[attr].theme_toggle_2;
-    //}
+    
     const currentLang = langEl.querySelector('.active').getAttribute('language');
     if (body.classList.contains('dark-mode')) {
             //themeToggleBtn.textContent = "Light Mode";//Dark mode active – show Light Mode option
@@ -72,8 +58,7 @@ function toggleTheme(){
         // Listen for button clicks and call toggleTheme function
 themeToggleBtn.addEventListener('click', toggleTheme);
 
-        
-        //main logic start here
+//main logic start here
 links.forEach(el => {
     el.addEventListener('click', () => {
         langEl.querySelector('.active').classList.remove('active');
@@ -85,6 +70,7 @@ links.forEach(el => {
         aboutMe_li.textContent    = data[lang].aboutMe_li_;
         skills_li.textContent     = data[lang].skills_li_;
         portfolio_li.textContent  = data[lang].portfolio_li_;
+        games_li.textContent     = data[lang].games_li_;
         videos_li.textContent     = data[lang].videos_li_;
         contacts_li.textContent   = data[lang].contacts_li_;
         //about section
@@ -100,6 +86,8 @@ links.forEach(el => {
         portfolioH3_3.textContent = data[lang].portfolioH3_3_;
         //videos section
         videosH2.textContent      = data[lang].videosH2_;
+        //games section
+        gamesH2.textContent      = data[lang].gamesH2_;
         //contacts section
         contactsH2.textContent    = data[lang].contactsH2_;
         contactsBtn.textContent   = data[lang].contactsBtn_;
@@ -125,6 +113,7 @@ fetch('./data.json')
     aboutMe_li.textContent    = data[defaultLang].aboutMe_li_;
     skills_li.textContent     = data[defaultLang].skills_li_;
     portfolio_li.textContent  = data[defaultLang].portfolio_li_;
+    games_li.textContent     = data[defaultLang].games_li_;
     videos_li.textContent     = data[defaultLang].videos_li_;
     contacts_li.textContent   = data[defaultLang].contacts_li_;
     //about section 
@@ -140,6 +129,8 @@ fetch('./data.json')
     portfolioH3_3.textContent = data[defaultLang].portfolioH3_3_;
     //videos section
     videosH2.textContent      = data[defaultLang].videosH2_;
+    //games section
+    gamesH2.textContent      = data[defaultLang].gamesH2_;
     //contacts section
     contactsH2.textContent    = data[defaultLang].contactsH2_;
     contactsBtn.textContent   = data[defaultLang].contactsBtn_;
